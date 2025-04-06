200f871f4f101e38176cfc0fa77ea0013db5798c
YogyashriPatil <yogyashripatil20@gmail.com>
2025-04-06 13:25:54 +0530

    Project completed

--- backend/src/index.js
+++ backend/src/index.js
@@ -23,9 +23,6 @@ app.use(cors({
     credentials:true,
     }
 ));
-// app.use(express.json({limit:"10mb"}));
-// app.use(express.urlencoded({ limit: "10mb", extended: true })); // Increase URL-encoded data size
-// app.use(cookieParser());
 
 app.use("/api/auth",authRoutes);
 app.use("/api/messages",messageRoutes);
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:5001/api", // Ensure this is your backend URL
+  baseURL: import.meta.env.MODE ==="development" ? "http://localhost:5001/api":"/api", // Ensure this is your backend URL
   withCredentials: true,  // Allow credentials like cookies
 });
 
